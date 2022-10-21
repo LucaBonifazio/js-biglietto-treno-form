ee0c94317ea16e04306ba53182ff13730ec005c4
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-10-21 16:45:48 +0200

    Work on discount

--- js/script.js
+++ js/script.js
@@ -2,23 +2,26 @@ const eleName = document.querySelector('#name');
 const eleKm = document.querySelector('#km');
 const eleAge = document.querySelector('#age');
 const eleBox = document.querySelector('box_info')
-const eleConferme = document.querySelector('.btn-success');
 const eleBoxTicket = document.querySelector('.box_ticket')
-const Km = parseFloat();
-
-const basePrice = parseFloat(Km * 0.21); 
-let discount = 0;
-
-if (age < 18) {
-    discount = 20
-} else if (age >= 65) {
-    discount = 40
-}
-
-let totalPrice = basePrice - basePrice * discount / 100;
-totalPrice = parseFloat(totalPrice.toFixed(2));
+const eleConferme = document.querySelector('.btn-success');
+const eleDetelete = document.querySelector('.btn-danger');
 
 eleConferme.addEventListener('click', function () {
-	console.log('Conferma cliccata');
-	eleBoxTicket.classList.add('box_ticket');
+    const Km = parseFloat(eleKm.value);
+    const basePrice = parseFloat(Km * 0.21); 
+    let discount = 0;
+
+    if (age < 18) {
+        discount = 20
+    } else if (age >= 65) {
+        discount = 40
+    }
+
+    let totalPrice = basePrice - basePrice * discount / 100;
+    totalPrice = parseFloat(totalPrice.toFixed(2));
+    eleBoxTicket.innerHTML = (`Questo è il prezzo del tuo bigletto: ${totalPrice} €. Fai buon viaggio!` )
+});
+
+eleDetelete.addEventListener('click', function () {
+
 });
